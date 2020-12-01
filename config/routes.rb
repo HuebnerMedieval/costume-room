0d5a03feeff38cc0ba8bc075f3d2cecb9345dfe2
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-01 10:35:59 -0500

    adds basic nav bar with signup/login or logout depending on session status

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "application#home"
+  devise_for :users, :controllers => {:registrations => "registrations"}
+    #change route from /users/login to /login route
+    devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+    end
+
+    devise_scope :user do
+      get 'signup', to: 'devise/registrations#new'
+    end
+    
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: "application#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_01_142206) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
