a2daabe7e3c58241a3f3d31ceecb3e45394e98e0
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-02 10:39:45 -0500

    sets up actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,14 @@
 class ActorsController < ApplicationController
+
+    def show
+        if current_user.actors.find_by(id: params[:id])
+            @actor = current_user.actors.find_by(id: params[:id])
+        else
+            redirect_to actors_path
+        end
+    end
+
+    def index
+        @actors = current_user.actors
+    end
 end
