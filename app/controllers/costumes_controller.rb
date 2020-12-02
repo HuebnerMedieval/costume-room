c9004b891d3bbec24f57341b31fb881b6800695c
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-02 11:00:18 -0500

    adds blank index, show, new, and update views for costumes, and adds routes for them

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,2 +1,38 @@
 class CostumesController < ApplicationController
+
+    def index
+        @costumes = current_user.costumes
+    end
+
+    def show
+        if current_user.costumes.find_by(id: params[:id])
+            @costume = current_user.costumes.find_by(id: params[:id])
+        else
+            redirect_to costumes_path
+        end
+    end
+
+    def new
+        @costume = Costume.new
+    end
+
+    def create
+        #to fill out
+    end
+
+    def edit
+        if current_user.costumes.find_by(id: params[:id])
+            @costume = current_user.costumes.find_by(id: params[:id])
+        else
+            redirect_to costumes_path
+        end
+    end
+
+    def update
+        #to fill out
+    end
+
+    def destroy
+        #to fill out
+    end
 end
