7d535bba22784c53ab6445ee7961aef8692125a9
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-02 10:16:45 -0500

    removes user controller and show route

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < Devise::UsersController
-    def show
-        if user_signed_in?
-            @user = current_user
-        else
-            redirect_to root_path
-        end
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
 
     devise_scope :user do
       get 'signup', to: 'devise/registrations#new'
-      get 'users/:id', to: 'users#show', as: 'user'
     end
     
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
