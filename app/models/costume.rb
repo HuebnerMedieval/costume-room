9cd5009f29501ec5ba6b5ba83d7d6b22b6c9fd73
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-03 16:46:03 -0500

    adds scope methods for costumes and filters costumes on actor show page to only those belonging to the user

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -4,6 +4,11 @@ class Costume < ApplicationRecord
 
     accepts_nested_attributes_for :actor
 
+    scope :authentic, -> {where(authentic: true)}
+    scope :fantasy, -> {where(authentic: false)}
+    scope :finished, -> {where(finished: true)}
+    scope :wip, -> {where(finished: false)}
+
     def actor_attributes=(actor_attributes)
         if actor_attributes["name"] != ""
             actor_attributes.values.each do |actor_attribute|
