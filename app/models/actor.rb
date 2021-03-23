6f82c2a3edc86663961dbc17ac2ace867736c99d
Eli Huebner <elimhuebner@gmail.com>
2021-03-23 12:56:44 -0400

    adds comments to actor.rb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,9 @@
 class Actor < ApplicationRecord
+    # sets has_many relationship with costumes
     has_many :costumes
+    # sets has_many through relationship with user
     has_many :users, through: :costumes
 
+    # scope method to allow users to search for a particular actor. Returns a list of actors whose names are similar to the search term
     scope :found, -> (name) {where("name LIKE ?", "%#{name}%" )}
 end
