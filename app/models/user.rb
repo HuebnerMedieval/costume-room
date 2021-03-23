71fc9585c08811f25c7c5377d58b5961276bd92d
Eli Huebner <elimhuebner@gmail.com>
2021-03-23 13:05:32 -0400

    adds comments to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   
+  # sets associations for users
   has_many :costumes
   has_many :actors, through: :costumes
   
 
+  # gets credentials from omniauth
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
