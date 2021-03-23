eb679888023462b77941cba12545fd642be3b69d
Eli Huebner <elimhuebner@gmail.com>
2021-03-23 12:54:45 -0400

    adds comments to registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
     
     private
 
+    # sets strong params for the signup
     def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    # sets strong params for changing password
     def account_update_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
     end
