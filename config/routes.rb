1a9d7ec6833a4a37ecbeab01746d1c024daae628
Eli Huebner <elimhuebner@gmail.com>
2021-03-23 13:08:23 -0400

    adds comments for routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # enables all routes for costumes
   resources :costumes
 
+  # enables show and index routes for actors, and nested show, new, and edit routes for costumes
   resources :actors, only: [:show, :index] do
     resources :costumes, only: [:show, :new, :edit]
   end
 
+  # allows devise to handle authentication
   devise_for :users, :controllers => {registrations: "registrations", omniauth_callbacks: "callbacks"}
     #change route from /users/login to /login route
     devise_scope :user do
