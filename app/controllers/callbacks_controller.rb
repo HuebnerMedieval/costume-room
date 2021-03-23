ac05b2f864495ad27df1564d54d830e34727f0ec
Eli Huebner <elimhuebner@gmail.com>
2021-03-23 12:31:12 -0400

    adds comment to callbacks_controller.rb

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,5 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    
+    # uses OmniAuth to get user info from GitHub
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
